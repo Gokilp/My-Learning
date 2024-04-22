c9a29f172523bb77e622581794ab6e0f815092af
gokilp <gokilp25@gmail.com>
2024-04-22 10:35:09 +0530

    arrays

---
+++ Arrays.js
@@ -0,0 +1,21 @@
+// arrays indexing
+
+let cities = ["chennai", "mudurai", "coimbatore", "kerla", "chennai"];
+
+console.log(cities[cities.length - 3]);
+
+// mix numbers and strings
+let arr = [1, 2, 3, 4, 5, [6, 7], "a", "gokil"];
+console.log(arr[5][0]);
+
+// 2d arrays
+let arr1 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 13],
+];
+
+console.log("2d arrays : " + arr1[2][2]);
+
+
+// push 
