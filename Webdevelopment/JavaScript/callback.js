993007b48a7808f622e319534435c4a676beeb14
gokilp <gokilp25@gmail.com>
2024-04-24 09:26:59 +0530

    call backs

--- Webdevelopment/JavaScript/callback.js
+++ Webdevelopment/JavaScript/callback.js
@@ -1,11 +1,53 @@
-function say() {
-  console.log("Good Morning");
-  sayhello();
+//callback - Function passed as argument
+
+function greetConsole(name) {
+  console.log("hello", name);
 }
 
-function sayhello() {
-  console.log("good Day");     // good day Good Morning  Good Morning  good day
+function greetHeading(name) {
+  const heading = document.querySelector("h1");
+  heading.innerHTML = "hello " + name;
 }
 
+function greet(callback) {
+  console.log(callback);
+  callback("Ram");
+}
 
-say(sayhello());
+greet(greetConsole);
+// greet(greetHeading)
+
+greet((name) => {
+  const heading = document.querySelector("h1");
+  heading.innerHTML = "hello " + name;
+});
+console.clear();
+
+//Foreach
+arr = ["deepa", "suresh", "ramya"];
+
+arr.forEach(print);
+
+function print(val) {
+  console.log(val.toUpperCase());
+}
+
+arr.forEach((val) => console.log(val.toUpperCase()));
+console.log(arr);
+
+arr.forEach((val, index, arr) => {
+  arr[index] = val.toUpperCase();
+});
+
+console.log(arr);
+
+arr = ["ECE", "IT", "CSC", "EEE"];
+
+arr.forEach((val) => {
+  const opt = document.createElement("option");
+  opt.textContent = val;
+  opt.value = val;
+  document.getElementById("branch").appendChild(opt);
+});
+
+console.clear();
