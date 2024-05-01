8a2708a31bfac1dd6104d1f3cc176b9cb9fb1a3a
Gokil P <76507378+Gokilp@users.noreply.github.com>
2024-05-01 19:51:12 +0530

    Update session.php
    add home.php file and  hello.php file added

--- Php/session.php
+++ Php/session.php
@@ -1,1 +1,64 @@
+<!---->
+<!---->
+<!-- // session => SGB used to store information-->
+<!--//             to be Store across Multiple pages.-->
+<!--//             A user is assignment a session -id-->
+<!--//                Eg:Login creditals-->
+<?php
+
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+       <form actiom="hello.php" method = "post">
+           Username <br>
+           <input type="text" name="Username"> <br>
+             Password <br>
+           <input type="password" name="password"> <br>
+           <input type="submit" name="login" value="login">
+
+       </form>
+</body>
+</html>
+<?php
+
+if (isset($_POST["login"])) {
+    if (!empty($_POST["Username"]) && !empty($_POST["password"])) {
+        $_SESSION["Username"] = $_POST["Username"];
+        $_SESSION["password"] = $_POST["password"];
+
+        header("Location:home.php");
+    } else {
+        echo "Invalid Username or Password";
+    }
+}
+?>
+
+// hello php code 
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p> This is Loin  Page</p> <br>
+<a href="hello.php"> This is login page</a>
+</body>
+</html>
+<?php
+echo $_SESSION["Username"] ;
+echo $_SESSION["password"];
+?>
 
