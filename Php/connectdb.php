d3fd094eec848fc30b2e486beaf86a5c98d4cc6c
Gokil P <76507378+Gokilp@users.noreply.github.com>
2024-05-02 11:27:37 +0530

    Update connectdb.php

--- Php/connectdb.php
+++ Php/connectdb.php
@@ -1,39 +1,38 @@
-// database.php file
-
 <?php
-  $db_server = "localhost";      // connected to db 
-  $db_username = "root";
-  $db_password = "";
-  $db_name = "bussinessdatabase";
-  $conn =" ";
+include("database.php"); // redirectio to anthoer php file
+
+$username = "Gokilp"; // vraiable stored 
+$password = "12334456"; // variable stored 
+
+$sql = "INSERT INTO users (`user`, `password`) VALUES ('$username', '$password')";  // added sql command 
 
 try {
-    $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);  // passing parameter to the db
+    if (mysqli_query($conn, $sql)) {
+        echo "Record inserted successfully.";
+    } else {
+        throw new Exception("Error inserting record: " . mysqli_error($conn));
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+mysqli_close($conn);
+?>
+
+
+// database.php
+<?php
+$db_server = "localhost";   // decalre of php file of command
+$db_username = "root";
+$db_password = "";
+$db_name = "bussinessdatabase";  // give the db name  correctly of decalre 
+$conn =" ";
+
+try {
+    $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);  // send  db 
 } catch (mysqli_sql_exception ) {
     echo "Connection failed: ";
 }
 
 
 ?>
-
-
-// index.php
-
-<?php
-     include("database.php");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-      <p>hello ! Welcome to the World</p>
-<body>
-
-</body>
-
-</html>
