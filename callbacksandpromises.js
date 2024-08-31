4cfeb5527f46871c60c4e3dc95887dbd344e95e3
Gokil P <76507378+Gokilp@users.noreply.github.com>
2024-08-31 15:16:55 +0530

    Update callbacksandpromises.js

--- callbacksandpromises.js
+++ callbacksandpromises.js
@@ -1,95 +1,30 @@
-function settimeoutpromised (duration){
-
- return new Promise (function (reslove){
-
-//  setTimeout(reslove,duration);
-   console.log(" promised print")
-
- });
-}
-
-
-function Callback(){
-    console.log(" 5 Second Sloved");
-}
-
-settimeoutpromised(5000).then(Callback);
-
-// -----------------------  Call back ---------------------------
-setTimeout(()=>{
-
-    console.log("Hi");
-
- setTimeout(()=>{
-    console.log("Hello ");
-
-     setTimeout(()=>{
-         console.log("Hello World");
-     },5000)
- },2000)
-},1000)
-
-// Code for write in Promise 
-
-
-function promised (message,duration){
-  
-    return new Promise(function (reslove){
- 
- setTimeout(()=>{
-    console.log(message);
-    reslove();
- },duration);
-});
-
-}
-promised("Hi",1000)
-.then(()=>{
-    promised("Hello",2000);
-})
-.then(()=>{
-    promised("Hello World",5000);
-})
-
-
-// -------------------------------------------- Call back -------------------------------
-
-
-
-
-
+// call backs
 
 setTimeout(()=>{
-   console.log("hello");
-  
-   setTimeout(()=>{
-   console.log("hi");
-   setTimeout(()=>{
-      console.log("hi there");
-   },5000);
-   },2000)
+  console.log("hi");
+  setTimeout(()=>{
+    console.log("Good Programming");
+    setTimeout(()=>{
+         console.log("Good Morning");
+    },5000)
+  },2000)
 },1000)
 
-// --------- Promise _ --------------------------------
 
 
-function settimeoutpromised(message,Time){
-
-
-    return new Promise ((reslove)=>{
-
-     setTimeout(()=>{
-         console.log(message);
-         reslove();
-     },Time);
-
-    })
+function Step3 (){
+     console.log("Good Morning")
 }
 
-settimeoutpromised("hi",1000)
-.then(()=>{
-   settimeoutpromised("Hello",2000)
-})
-.then(()=>{
-      settimeoutpromised("Hello World",5000)
-})
+function Step2(){
+    console.log(" Good Programming");
+   setTimeout(Step3,5000)
+}
+
+function Step1(){
+    console.log("hi");
+    setTimeout(Step2,2000);
+}
+
+setTimeout(Step1,1000)
+  
